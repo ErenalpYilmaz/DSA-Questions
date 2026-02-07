96c930c4ca334e721094d21c9699859e24b0441b
ERENALP YILMAZ <160880865+ErenalpYilmaz@users.noreply.github.com>
2026-02-07 15:04:46 +0300

    Time: 0 ms (100%), Space: 19.3 MB (51.79%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -5,7 +5,6 @@ class Solution:
             return True
         if word.isupper() == True:
             return True
-        
         for words in range(1, len(word)):
             if word[words].isupper() == True:
                 isTrue = False
